d74b4ed023547c7d9e6a1c291dd898515fd8eaaa
Nitz <58024520+nitinsali@users.noreply.github.com>
2024-04-08 13:16:52 +0530

    Update config.py
    GTP4 Endpoints added

--- config.py
+++ config.py
@@ -10,6 +10,8 @@ import os
 class DefaultConfig:
     """ Bot Configuration """
 
-    PORT = 8000
+    PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "d6461019-4923-46af-9f52-f81a0b16486a")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "Hgc8Q~EIJVtFxhv50fdm~oSWLnkF3J.SghLYBcEd")
+    GPT4V_ENDPOINT = os.environ.get("GPT4V_ENDPOINT", "https://holmes-genopenai.openai.azure.com/openai/deployments/gpt4-Vision/chat/completions?api-version=2024-02-15-preview")
+    GPT4V_KEY = os.environ.get("GPT4V_KEY", "d36dbc6b61154c4c8ca62f2c2cc386a3")
